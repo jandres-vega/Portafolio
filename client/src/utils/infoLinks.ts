9b7a477dc05cb86f3cd0566cdbd03eec15cb7900
jandres-vega <jandresvega@itsa.edu.co>
2023-06-05 20:07:21 -0500

    Feact: create array of lsit by navBar

---
+++ client/src/utils/infoLinks.ts
@@ -0,0 +1,26 @@
+import List from '../types/listNavBar.ts';
+
+const listNavBar: List[] = [
+    {
+        id: 1,
+        link: "Inicio"
+    },
+    {
+        id: 2,
+        link: "Proyectos"
+    },
+    {
+        id: 3,
+        link: "Certificados"
+    },
+    {
+        id: 4,
+        link: "Tecnologias"
+    },
+    {
+        id: 5,
+        link: "Contact"
+    }
+]
+
+export {listNavBar}
