33a5424a0f09ce670ed2a23a9a78cb4d1e6b9abd
jandres-vega <jandresvega@itsa.edu.co>
2023-06-06 19:28:45 -0500

    Feact: add config a taiwin y quit scroll body

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'dark-blue': '#1D2A40',
+      'white': '#FFFFFF',
+      'purple': '#50628D',
+      'red': '#880505'
+    },
     extend: {},
   },
   plugins: [],
