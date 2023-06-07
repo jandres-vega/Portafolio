65730aa42bd64fe86e1acbedd3daed46b911e0f3
jandres-vega <jandresvega@itsa.edu.co>
2023-06-06 20:18:20 -0500

    Feact: add img of user

--- client/src/components/ProfileUser.tsx
+++ client/src/components/ProfileUser.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {CustomImg} from "../common/CustomImg.tsx";
-import imgSinFoto from '../assets/sin foto de perfil.jpg'
+
 const ProfileUser:React.FC = () => {
     return (
         <div className="flex justify-center gap-20">
             <div className="m-3">
-                <CustomImg className="rounded-full" src={imgSinFoto} alt="imagen sin foto" />
+                <CustomImg className="rounded-full max-w-xs" src="https://res.cloudinary.com/cloudmulti/image/upload/v1686098790/Portafolio/profile_lqxzpw.png" alt="imagen sin foto" />
             </div>
             <div className="flex items-center">
                 <h1 className="text-4xl">Desarrollador Web Full-Stack</h1>
--- client/src/routes/App.tsx
+++ client/src/routes/App.tsx
@@ -3,6 +3,11 @@ import {HashRouter, Routes, Route} from 'react-router-dom';
 import {Header} from "../components/Header.tsx";
 import {LandingPage} from "../pages/LandingPage.tsx";
 import {Layout} from "../containers/Layout.tsx";
+import {Projects} from "../pages/Projects.tsx";
+import {Certificates} from "../pages/Certificates.tsx";
+import {Technologies} from "../pages/Technologies.tsx";
+import {Contact} from "../pages/Contact.tsx";
+import {NotFound} from "../pages/NotFound.tsx";
 const App: React.FC = () => {
     return (
         <HashRouter>
@@ -10,6 +15,11 @@ const App: React.FC = () => {
             <Layout>
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/certificates" element={<Certificates />} />
+                    <Route path="/technologies" element={<Technologies />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<NotFound/>} />
                 </Routes>
             </Layout>
         </HashRouter>
