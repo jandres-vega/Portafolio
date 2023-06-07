a57834307013370a07feaa0828db44152bcafee5
jandres-vega <jandresvega@itsa.edu.co>
2023-06-06 19:27:34 -0500

    Feact: add NaV

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 // import {NavLink} from "react-router-dom";
 import {listNavBar} from '../utils/infoLinks.ts';
 const NavBar:React.FC = () => {
-    // const styleActive = "underline underline-offset-4"
+    //const styleActive = "underline underline-offset-4"
     return (
         <nav>
             <ul className="flex items-center gap-10">
                 {
                     listNavBar.map(item => (
-                        <li className="font-semibold text-white" key={item.id}>
+                        <li className="font-semibold text-white cursor-pointer rounded-2xl px-2" key={item.id}>
                             {item.link}
                         </li>
                     ))
