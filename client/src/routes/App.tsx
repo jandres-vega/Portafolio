d78408c859f7f11e0d88eb040472f53af20cc860
jandres-vega <jandresvega@itsa.edu.co>
2023-06-06 19:29:29 -0500

    Feact: created structure de router

--- client/src/routes/App.tsx
+++ client/src/routes/App.tsx
@@ -1,8 +1,18 @@
-const App = () => {
+import React from "react";
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import {Header} from "../components/Header.tsx";
+import {LandingPage} from "../pages/LandingPage.tsx";
+import {Layout} from "../containers/Layout.tsx";
+const App: React.FC = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Portafolio
-        </h1>
+        <HashRouter>
+            <Header />
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </Layout>
+        </HashRouter>
     );
 };
 export {App};
